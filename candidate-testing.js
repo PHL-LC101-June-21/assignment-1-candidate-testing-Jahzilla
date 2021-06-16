fb229c8b4a7e282362bc402e5aaed616f3bac5a0
Jahzilla <85647688+Jahzilla@users.noreply.github.com>
2021-06-16 16:24:06 +0000

    Asked the rest of the questions stored in the questions array, and stored the answers in the candidate Answers array. Then referenced whether they matched and output to console the comparison

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,5 +1,6 @@
 const input = require('readline-sync');
 
+
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
@@ -8,9 +9,9 @@ let candidateName = "";
 let question = "Who was the first American woman in space?";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers =[];
 
 
 function askForName() {
@@ -20,18 +21,50 @@ candidateName = input.question("What is your Name?");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+candidateAnswers.push(input.question(questions[0]));
+console.log("\n");
+candidateAnswers.push(input.question(questions[1]));
+console.log("\n");
+candidateAnswers.push(input.question(questions[2]));
+console.log("\n");
+candidateAnswers.push(input.question(questions[3]));
+console.log("\n");
+candidateAnswers.push(input.question(questions[4]));
+console.log("\n");
+
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if(candidateAnswer === "Sally Ride"){
-  console.log("That is correct!");
+if(candidateAnswers[0] === correctAnswers[0]){
+  console.log(`\nWhen asked "${questions[0]}", you answered: \n"${candidateAnswers[0]}." \nThat is correct!`);
 } else {
-  console.log("I'm sorry that is not the correct answer.");
+  console.log(`\nWhen asked "${questions[0]}", you answered:\n${candidateAnswers[0]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[0]}.`);
 }
+if(candidateAnswers[1] === correctAnswers[1]){
+  console.log(`\nWhen asked "${questions[1]}", you answered: \n"${candidateAnswers[1]}." \nThat is correct!`);
+} else {
+  console.log(`\nWhen asked "${questions[1]}", you answered:\n${candidateAnswers[1]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[1]}.`);
+}
+if(candidateAnswers[2] === correctAnswers[2]){
+  console.log(`\nWhen asked "${questions[2]}", you answered: \n"${candidateAnswers[2]}." \nThat is correct!`);
+} else {
+  console.log(`\nWhen asked "${questions[2]}", you answered:\n${candidateAnswers[2]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[2]}.`);
+}
+if(candidateAnswers[3] === correctAnswers[3]){
+  console.log(`\nWhen asked "${questions[3]}", you answered: \n"${candidateAnswers[3]}." \nThat is correct!`);
+} else {
+  console.log(`\nWhen asked "${questions[3]}", you answered:\n${candidateAnswers[3]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[3]}.`);
+}
+if(candidateAnswers[4] === correctAnswers[4]){
+  console.log(`\nWhen asked "${questions[4]}", you answered: \n"${candidateAnswers[4]}." \nThat is correct!`);
+} else {
+  console.log(`\n When asked "${questions[4]}", you answered:\n${candidateAnswers[4]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[4]}.`);
+}
+
+
 
   let grade;
   
@@ -42,7 +75,7 @@ if(candidateAnswer === "Sally Ride"){
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Hello, " + candidateName + "!");
+  console.log("\nHello, " + candidateName + "!\n");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
