5eb4bd43784560cdce11fb5347de2a5b7417c1c9
Jahzilla <85647688+Jahzilla@users.noreply.github.com>
2021-06-28 03:26:05 +0000

    Completed part 3 and finished the full assignment

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,6 +12,8 @@ let candidateAnswer = "";
 let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers =[];
+let rightAnswers = 0
+
 
 
 function askForName() {
@@ -20,54 +22,44 @@ candidateName = input.question("What is your Name?");
 }
 
 function askQuestion() {
+  for(let i = 0; i < questions.length; i++){
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswers.push(input.question(questions[0]));
-console.log("\n");
-candidateAnswers.push(input.question(questions[1]));
-console.log("\n");
-candidateAnswers.push(input.question(questions[2]));
-console.log("\n");
-candidateAnswers.push(input.question(questions[3]));
-console.log("\n");
-candidateAnswers.push(input.question(questions[4]));
-console.log("\n");
+  candidateAnswers.push(input.question(questions[i]));
+  console.log("\n");
+  }
+
 
 
 }
 
 function gradeQuiz(candidateAnswers) {
+  for (let i = 0; i < questions.length; i++){
+      if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+      console.log(`\nWhen asked "${questions[i]}", you answered: \n"${candidateAnswers[i]}." \nThat is correct!`);
+      rightAnswers += 1;
+      
+      } else {
+      console.log(`\nWhen asked "${questions[i]}", you answered:\n${candidateAnswers[i]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[i]}.`);
+      
+  }
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if(candidateAnswers[0] === correctAnswers[0]){
-  console.log(`\nWhen asked "${questions[0]}", you answered: \n"${candidateAnswers[0]}." \nThat is correct!`);
-} else {
-  console.log(`\nWhen asked "${questions[0]}", you answered:\n${candidateAnswers[0]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[0]}.`);
-}
-if(candidateAnswers[1] === correctAnswers[1]){
-  console.log(`\nWhen asked "${questions[1]}", you answered: \n"${candidateAnswers[1]}." \nThat is correct!`);
-} else {
-  console.log(`\nWhen asked "${questions[1]}", you answered:\n${candidateAnswers[1]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[1]}.`);
-}
-if(candidateAnswers[2] === correctAnswers[2]){
-  console.log(`\nWhen asked "${questions[2]}", you answered: \n"${candidateAnswers[2]}." \nThat is correct!`);
-} else {
-  console.log(`\nWhen asked "${questions[2]}", you answered:\n${candidateAnswers[2]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[2]}.`);
-}
-if(candidateAnswers[3] === correctAnswers[3]){
-  console.log(`\nWhen asked "${questions[3]}", you answered: \n"${candidateAnswers[3]}." \nThat is correct!`);
-} else {
-  console.log(`\nWhen asked "${questions[3]}", you answered:\n${candidateAnswers[3]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[3]}.`);
-}
-if(candidateAnswers[4] === correctAnswers[4]){
-  console.log(`\nWhen asked "${questions[4]}", you answered: \n"${candidateAnswers[4]}." \nThat is correct!`);
-} else {
-  console.log(`\n When asked "${questions[4]}", you answered:\n${candidateAnswers[4]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[4]}.`);
+
 }
 
 
 
-  let grade;
+
+
+  let grade = (rightAnswers / (questions.length)) * 100;
   
+  
+  if(grade >= 80){
+    console.log("\nCongratulations you passed!");
+  } else {
+    console.log("\nI'm sorry you did not answer enough questions correctly. You did not pass.");
+  }
+  console.log(`\nYou scored ${grade}%. (${rightAnswers} of ${questions.length} correct)`);
 
   return grade;
 }
