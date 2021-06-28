8b8da9d594a1347ca1ff8f525de2d64836cfd4ef
Jahzilla <85647688+Jahzilla@users.noreply.github.com>
2021-06-28 23:16:17 +0000

    reviewed questions array for spacing in each item

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,23 +9,23 @@ let candidateName = "";
 let question = "Who was the first American woman in space?";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
-let candidateAnswers =[];
+let candidateAnswers = [];
 let rightAnswers = 0
 
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your Name?");
+  candidateName = input.question("What is your Name?");
 }
 
 function askQuestion() {
-  for(let i = 0; i < questions.length; i++){
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswers.push(input.question(questions[i]));
-  console.log("\n");
+  for (let i = 0; i < questions.length; i++) {
+    // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+    candidateAnswers.push(input.question(questions[i]));
+    console.log("\n");
   }
 
 
@@ -33,28 +33,28 @@ function askQuestion() {
 }
 
 function gradeQuiz(candidateAnswers) {
-  for (let i = 0; i < questions.length; i++){
-      if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+  for (let i = 0; i < questions.length; i++) {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
       console.log(`\nWhen asked "${questions[i]}", you answered: \n"${candidateAnswers[i]}." \nThat is correct!`);
       rightAnswers += 1;
-      
-      } else {
+
+    } else {
       console.log(`\nWhen asked "${questions[i]}", you answered:\n${candidateAnswers[i]}\nI'm sorry that is incorrect. \nThe correct answer is ${correctAnswers[i]}.`);
-      
+
+    }
+
+    // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
   }
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-}
 
 
 
 
 
   let grade = (rightAnswers / (questions.length)) * 100;
-  
-  
-  if(grade >= 80){
+
+
+  if (grade >= 80) {
     console.log("\nCongratulations you passed!");
   } else {
     console.log("\nI'm sorry you did not answer enough questions correctly. You did not pass.");
